4f5c09a04105a190badcdc33a348cc71cf2daea8
JamesGardner01 <69728604+JamesGardner01@users.noreply.github.com>
2020-08-15 19:12:30 +0100

    Add files via upload

---
+++ Board.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int redValue;
+    public int blueValue;
+    public int yellowValue;
+    public int greenValue;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
