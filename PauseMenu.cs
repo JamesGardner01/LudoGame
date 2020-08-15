362d2ab8643936ac4cb61adab4d3a6df63cd6504
JamesGardner01 <69728604+JamesGardner01@users.noreply.github.com>
2020-08-15 19:18:20 +0100

    Add files via upload

---
+++ PauseMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool gamePaused;
+    public GameObject mainPauseMenu;
+    public GameObject rulesMenu;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused == false)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        gamePaused = true;
+    }
+
+    public void Resume()
+    {
+        rulesMenu.SetActive(false);
+        mainPauseMenu.SetActive(true);
+
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+        gamePaused = false;
+    }
+
+    public void Rules()
+    {
+        Debug.Log("Rules");
+        mainPauseMenu.SetActive(false);
+        rulesMenu.SetActive(true);
+    }
+
+    public void Back()
+    {
+        rulesMenu.SetActive(false);
+        mainPauseMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        SceneManager.LoadScene(0);
+    }
+}
