0a1df8adc72282d585bb9ea4d13cb155621258f1
JamesGardner01 <69728604+JamesGardner01@users.noreply.github.com>
2020-08-15 19:19:02 +0100

    Add files via upload

---
+++ StartScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject ruleScreen;
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void RuleButton()
+    {
+        mainMenu.SetActive(false);
+        ruleScreen.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        ruleScreen.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
