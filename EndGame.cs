c76c175f2246d10dad5669a7b5368458d94df0c7
JamesGardner01 <69728604+JamesGardner01@users.noreply.github.com>
2020-08-15 20:17:57 +0100

    Add files via upload

---
+++ EndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        SceneManager.LoadScene(0);
+    }
+}
