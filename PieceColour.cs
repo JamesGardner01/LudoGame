b3730fd81843c3fbd7e642e7080b195a09a6814c
JamesGardner01 <69728604+JamesGardner01@users.noreply.github.com>
2020-08-15 19:23:28 +0100

    Update PieceColour.cs
    This script is attached every players' pieces so that other scripts can easily know what colour the piece is.

--- PieceColour.cs
+++ PieceColour.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class PieceColour : MonoBehaviour
 {
+    //This script is attached every players' pieces so that other scripts can easily know what colour the piece is.
     public string colour;
 }
