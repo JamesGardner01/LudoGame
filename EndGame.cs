6a03f9c7d58ea3008c42ad1f91571d9298c83b2d
JamesGardner01 <69728604+JamesGardner01@users.noreply.github.com>
2020-08-15 20:18:33 +0100

    Update EndGame.cs
    This controls the quit to main menu button

--- EndGame.cs
+++ EndGame.cs
@@ -7,6 +7,7 @@ public class EndGame : MonoBehaviour
 {
     public void Quit()
     {
+        //This controls the quit to main menu button
         Debug.Log("Quit");
         SceneManager.LoadScene(0);
     }
